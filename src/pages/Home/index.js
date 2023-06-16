1f929f0ce35e07e0d13ab06785c075f875dae3cd
Roza Russkikh <rrrusskikh@gmail.com>
2023-06-16 15:25:00 -0700

    Fix a typo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,38 +1,48 @@
-import Planet from '../../assets/images/planet1.png'
-import Star from '../../assets/images/star.png'
-import './index.css';
+import Planet from "../../assets/images/planet1.png";
+import Star from "../../assets/images/star.png";
+import "./index.css";
 
 function Home() {
   return (
     <div>
       <div className="circle">
-        <img className='my-planet' src={Planet} alt="my-planet" />
-        <div className='starOrbit'>
-          <img className='star' src={Star} alt='star' />
-          <img className='star2' src={Star} alt='star2' />
+        <img className="my-planet" src={Planet} alt="my-planet" />
+        <div className="starOrbit">
+          <img className="star" src={Star} alt="star" />
+          <img className="star2" src={Star} alt="star2" />
         </div>
       </div>
-      <div className='about-me'>
+      <div className="about-me">
         <h2>Hello, I’m Roza and this is my learning space. </h2>
-        <div className='description'>
+        <div className="description">
           <p>
-            I’m passionate about building things for the web. I started my journey as a web developer
-            when I worked as a QA Engineer. I managed to create a web site that picks information to make a report which users can download as a .scv file,
-            and once that happened, I was completely hooked to the world of apps development. </p>
-          <p>I’ve cultivated my passion for coding by studying, but most of all, by building web apps using HTML, CSS, and JavaScript,
-            so that I could put learning into practice.</p>
-          <p>I’m now looking for a dev position
-            to finally kick off my career and learn among professionals.</p>
-          <p><span>
-            Here you can find books that I have been reading to gain knowledge. Also you can read my posts under Progress Journal
-            tab to know me better as a professional.
-          </span> </p>
-          <h2>Welcome to my space!
-          </h2>
+            I’m passionate about building things for the web. I started my
+            journey as a web developer when I worked as a QA Engineer. I managed
+            to create a web site that picks information to make a report which
+            users can download as a .csv file, and once that happened, I was
+            completely hooked to the world of apps development.{" "}
+          </p>
+          <p>
+            I’ve cultivated my passion for coding by studying, but most of all,
+            by building web apps using HTML, CSS, and JavaScript, so that I
+            could put learning into practice.
+          </p>
+          <p>
+            I’m now looking for a dev position to finally kick off my career and
+            learn among professionals.
+          </p>
+          <p>
+            <span>
+              Here you can find books that I have been reading to gain
+              knowledge. Also you can read my posts under Progress Journal tab
+              to know me better as a professional.
+            </span>{" "}
+          </p>
+          <h2>Welcome to my space!</h2>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Home;
